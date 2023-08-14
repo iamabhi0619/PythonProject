e4b9f0f861c0decaf7a47b659082539c5a26c2dd
Abhishek Kumar <131546128+iamabhi0619@users.noreply.github.com>
2023-08-14 11:00:50 +0530

    small change

--- Email-Sclice/code.py
+++ Email-Sclice/code.py
@@ -3,5 +3,7 @@ while True:
     if email_id.count('@') == 1 and len(email_id[:email_id.index('@')])>0 and len(email_id[email_id.index('@'):]):
         print("Your user name is:- ",email_id[:email_id.index('@')])
         print("Domain name:- ",email_id[email_id.index('@')+1:])
+    elif email_id == 'exit' :
+        break
     else:
         print("Enter a valid Email id..!!")
--- number-game/code.py
+++ number-game/code.py
@@ -1,51 +1,58 @@
-from random import randint
-print("GUESS THE NUMBER")
-print("Guess the number between 1 to 50..!!")
-print("""
-Easy Level:
-Difficulty: Easy
-Number of Chances: 10
-      
-Medium Level:
-Difficulty: Medium
-Number of Chances: 7
-      
-Hard Level:
-Difficulty: Hard
-Number of Chances: 4
-""")
+def main():
+    print("GUESS THE NUMBER")
+    print("Guess the number between 1 to 50..!!")
+    print("""
+    Easy Level:
+    Difficulty: Easy
+    Number of Chances: 10
 
-chance = 0
-ai = randint(1,50)
-def turn(level):
-    global chance
-    if(level == "easy"):
-        chance = 9
-    elif(level == "medium"):
-        chance = 6
-    elif(level == "hard"):
-        chance = 3
-    return chance
-def guess(ch,gn,ai):
-    while ch > 0:
-        if gn == ai:
-            print("You have gussed the number correctly..!!")
-            break
-        elif gn < ai:
-            print("You are on lower side")
-        elif gn > ai:
-            print("You are on higher side")
+    Medium Level:
+    Difficulty: Medium
+    Number of Chances: 7
 
-        gn = int(input("Trying gussing the number again:- "))
-        
-        ch -= 1  # Decrement chances
+    Hard Level:
+    Difficulty: Hard
+    Number of Chances: 4
+    """)
 
-    if ch == 0:
-        print("Out of chances. The number was:", ai)
-#value of chance
-level = input("Please choose your lavel:- ").lower()
-while level not in ("hard", "medium", "easy"):
-    level = input("Enter a valid choice(Hard/Medium/easy):- ").lower()    
-turn(level)
-ch = int(input("Enter the number you gussed:- "))
-guess(chance,ch,ai)
+    secret_number = randint(1, 50)
+    
+    def set_chances(difficulty_level):
+        difficulty_to_chances = {"easy": 10, "medium": 7, "hard": 4}
+        return difficulty_to_chances.get(difficulty_level, 0)
+    
+    message_by_difference = [
+        "You have guessed the number correctly..!!",
+        "You are very close!",
+        "You are very close!",
+        "You are getting closer.",
+        "You are getting closer.",
+        "You are getting closer.",
+        "You are way off!",
+        "You are way off!",
+        "You are way off!",
+        "You are way off!",
+        # ... and so on
+    ]
+    
+    def play_guessing_game(target_number):
+        chances_left = set_chances(difficulty_level)
+        while chances_left > 0:
+            guessed_number = int(input("Enter the number you guessed: "))
+            difference = abs(target_number - guessed_number)
+            if difference < len(message_by_difference):
+                print(message_by_difference[difference])
+                if difference == 0:
+                    break
+            print("You are on the lower side" if guessed_number < target_number else "You are on the higher side")
+            
+            chances_left -= 1
+    
+    difficulty_level = input("Please choose your level (easy/medium/hard): ").lower()
+    while difficulty_level not in ("hard", "medium", "easy"):
+        difficulty_level = input("Enter a valid choice (Hard/Medium/Easy): ").lower()
+    
+    play_guessing_game(secret_number)
+
+if __name__ == "__main__":
+    main()
